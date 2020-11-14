2c2f154d26d4364d80025dacabb5588b46856a9f
Maria Stager <73400027+Mstager@users.noreply.github.com>
2020-11-14 12:58:41 -0500

    Add files via upload

---
+++ genome_map.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SAMPLEID=`(ls *.fq.gz | sed 's/.fq.gz//g')` #name sample ID as filename of raw reads without .fastq extension
+
+
+REF=./GCA_003829775.1_ASM382977v1_genomic.fna #the location of the reference genome
+INPUT_DIR=./ #the input dir should have a /reads /trim and /sam folder
+
+
+
+for i in $SAMPLEID
+do echo "bwa for $i"
+bwa mem -t 10 ${REF} ${INPUT_DIR}/${i}.fq.gz > ${INPUT_DIR}/sam/${i}.sam
+done
+
+exit 0;
---
+++ genome_map_pe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SAMPLEID=`(ls *.rem.1.fq.gz | sed 's/.rem.1.fq.gz//g')` #name sample ID as filename of raw reads without .fastq extension
+
+
+REF=./GCA_003829775.1_ASM382977v1_genomic.fna #the location of the reference genome
+INPUT_DIR=./ #the input dir should have a /reads /trim and /sam folder
+
+
+
+for i in $SAMPLEID
+do echo "bwa for $i"
+bwa mem -t 20 ${REF} ${INPUT_DIR}/${i}.1.fq.gz ${INPUT_DIR}/${i}.2.fq.gz  > ${INPUT_DIR}/sam/${i}.paired.sam
+done
+
+exit 0;
